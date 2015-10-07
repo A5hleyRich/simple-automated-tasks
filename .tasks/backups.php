366a29a2b784d5937eec6b965c3df35a0aaad776
Ashley Rich <hello@ashleyrich.com>
2015-10-07 11:40:19 +0100

    Backup uploads dir

--- .tasks/backups.php
+++ .tasks/backups.php
@@ -2,16 +2,20 @@
 
 require_once './sites.php';
 
-$file = date( 'Y-m-d-His' ) . '-database.sql';
+$database_file = date( 'Y-m-d-His' ) . '-database.sql';
+$uploads_file = date( 'Y-m-d-His' ) . '-uploads.tar.gz';
 
 foreach ( $sites as $site ) {
 	chdir( $root . $site . '/public'  );
 	// Backup database
-	system( '/usr/local/bin/wp db export ../backups/' . $file . ' --add-drop-table' );
+	system( '/usr/local/bin/wp db export ../backups/' . $database_file . ' --add-drop-table' );
 	// Compress database file
-	system( 'gzip -f ../backups/' . $file );
+	system( 'gzip -f ../backups/' . $database_file );
+	# Backup uploads directory
+	system( 'tar -zcf ../backups/' . $uploads_file . ' wp-content/uploads' );
 	// Remove old backups
 	system( 'find ../backups -mtime +30 | xargs rm -fR' );
 	// Send to S3
-	system( '/usr/local/bin/aws s3 cp ../backups/' . $file . '.gz s3://' . $site . '/backups/ --storage-class REDUCED_REDUNDANCY' );
+	system( '/usr/local/bin/aws s3 cp ../backups/' . $database_file . '.gz s3://' . $site . '/backups/ --storage-class REDUCED_REDUNDANCY' );
+	system( '/usr/local/bin/aws s3 cp ../backups/' . $uploads_file . ' s3://' . $site . '/backups/ --storage-class REDUCED_REDUNDANCY' );
 }
