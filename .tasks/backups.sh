6dae054b58d93e9b51c202d92cba280ddf65a8e1
fabiangigler <fab.gigler@gmail.com>
2017-11-02 21:18:32 +0100

    adjust backup task

--- .tasks/backups.sh
+++ .tasks/backups.sh
@@ -3,23 +3,29 @@
 # Include config
 source /home/a5hley/.tasks/sites.sh
 
-# Filenames
-NOW=$(date +%Y-%m-%d-%H%M%S)
-DATABASE_FILE="${NOW}-database.sql";
-UPLOADS_FILE="${NOW}-uploads.tar.gz";
+
 
 for i in ${SITES[@]}
 do
-	cd "$ROOT/$i/public"
+
+	# Filenames
+	NOW=$(date +%Y-%m-%d)
+	DATABASE_FILE="$i-${NOW}-database.sql";
+	FILES_FILE="$i-${NOW}-files.tar.gz";
+
+	cd "$ROOT/$i"
 	# Backup database
 	/usr/local/bin/wp db export "../backups/$DATABASE_FILE" --add-drop-table
 	# Compress database file
 	gzip -f "../backups/$DATABASE_FILE"
-	# Backup uploads directory
-	tar -zcf "../backups/$UPLOADS_FILE" wp-content/uploads
+	# Backup all files directory
+	tar -zcf "../backups/$FIILES_FILE" ./
 	# Remove old backups
 	find ../backups -mtime +30 | xargs rm -fR
 	# Send to S3
-	/usr/local/bin/aws s3 cp "../backups/$DATABASE_FILE.gz" "s3://$i/backups/" --storage-class REDUCED_REDUNDANCY
-	/usr/local/bin/aws s3 cp "../backups/$UPLOADS_FILE" "s3://$i/backups/" --storage-class REDUCED_REDUNDANCY
+	# /usr/local/bin/aws s3 cp "../backups/$DATABASE_FILE.gz" "s3://$i/backups/" --storage-class REDUCED_REDUNDANCY
+	# /usr/local/bin/aws s3 cp "../backups/$UPLOADS_FILE" "s3://$i/backups/" --storage-class REDUCED_REDUNDANCY
+  # Send to Google Drive
+	# gdrive upload -p 0ByACnvoLyNYDRURlVGJkcnByc0U welthaus.staging.agentur.auftrieb.at.tar.gz
+
 done
